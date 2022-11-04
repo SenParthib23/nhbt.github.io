a4cbc5ac9a253488e567e0012f36c21e5fdcf6e8
SenParthib23 <parthibsen23@gmail.com>
2022-11-04 20:47:59 +0530

    nhbt website

---
+++ data2.js
@@ -0,0 +1,27 @@
+let movies = [
+    {
+        name:'falcon and the winter soldier',
+        des: 'Lorem ipsumolestias, recusandae voluptatem saepe inventore error dolorum.',
+        image: 'img/slider 2.png'
+    },
+    {
+        name:'loki',
+        des: 'Lorem ipsumolestias, recusandae voluptatem saepe inventore error dolorum.',
+        image: 'img/slider 1.png'
+    },
+    {
+        name:'wnada vision',
+        des: 'Lorem ipsumolestias, recusandae voluptatem saepe inventore error dolorum.',
+        image: 'img/slider 3.png'
+    },
+    {
+        name:'raya and the last dragon',
+        des: 'Lorem ipsumolestias, recusandae voluptatem saepe inventore error dolorum.',
+        image: 'img/slider 4.png'
+    },
+    {
+        name:'luca',
+        des: 'Lorem ipsumolestias, recusandae voluptatem saepe inventore error dolorum.',
+        image: 'img/slider 5.png'
+    }
+]
